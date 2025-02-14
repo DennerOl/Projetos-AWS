b698214c4d1ab612e8ae13c4db45a395df300cc0
DennerOl <dener155@hotmail.com>
2025-02-13 21:04:43 -0300

    criado projeto cdk e vpc

---
+++ curso_aws_cdk/src/main/java/com/myorg/VpcStack.java
@@ -0,0 +1,26 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+import software.amazon.awscdk.services.ec2.Vpc;
+
+public class VpcStack extends Stack {
+    public VpcStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public VpcStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        Vpc.Builder.create(this, "Vpc01")
+
+                .maxAzs(3) // Default is all AZs in region
+                .build();
+
+    }
+}
+
+// ver explicação no word voce anotou
